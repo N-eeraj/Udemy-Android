a2f440e73570ac9a159451cc2048431876af865f
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-22 19:22:06 +0530

    activity & layout

---
+++ ActivityLayout/app/src/main/java/com/example/activitylayout/MainActivity3.java
@@ -0,0 +1,14 @@
+package com.example.activitylayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout_activity);
+    }
+}
